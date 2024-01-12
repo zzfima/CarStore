eb4e06d4ce27cfeeaf5ba64b6a81c1b7432aefb7
zzfima <zzfima@gmail.com>
2024-01-12 11:00:38 +0200

    add nuget packages sqlite

---
+++ Server/CarsDBReaderEF.cs
@@ -0,0 +1,33 @@
+﻿using Server.Model;
+
+namespace Server
+{
+    public class CarsDBReaderEF : ICarsDBReader
+    {
+        public List<Manufacturer> ReadManufacturers()
+        {
+            List<Manufacturer> manufacturers = new List<Manufacturer>();
+
+            return manufacturers;
+        }
+
+        public List<BodyType> ReadBodyTypes()
+        {
+            List<BodyType> bodyTypes = new List<BodyType>();
+
+            return bodyTypes;
+        }
+
+        public List<Sample> ReadSamples(BodyType bodyType, Manufacturer manufacturer)
+        {
+            List<Sample> samples = new List<Sample>();
+
+            return samples;
+        }
+
+        public void WriteOrder(Sample selectedSample)
+        {
+
+        }
+    }
+}
